9f2f4d595ab6f8d42bf171b76b80a953a51c15b0
Raktim Shrestha <rakimsth@gmail.com>
2024-06-21 16:31:04 +0545

    added folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+
 const App = () => {
-  return <>Hello world</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/"></Route>
+        <Route path="/admin"></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ client/src/layouts/UserNavbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserNavbar = () => {
+  return <div>UserNavbar</div>;
+};
+
+export default UserNavbar;
